7af4257b42a1f4c3e4ca913147d9dad67083d625
Daniel Hjartland <squashical@gmail.com>
2023-10-14 21:47:39 +0200

    ch 4: 6. the profane

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"net/http"
+	"strings"
 )
 
 type apiConfig struct {
@@ -27,6 +29,7 @@ func main() {
 	apiRouter.Get("/healthz", handlerReadiness)
 	apiRouter.Get("/metrics", apiCfg.handlerMetrics)
 	apiRouter.Get("/reset", apiCfg.handlerReset)
+	apiRouter.Post("/validate_chirp", handlerValidateChirp)
 	router.Mount("/api", apiRouter)
 
 	adminRouter := chi.NewRouter()
@@ -53,4 +56,49 @@ func main() {
 	}
 	fmt.Printf("Server listening on port %s\n", port)
 	server.ListenAndServe()
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type cleanedBody struct {
+		Clean string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	cleanedText := censorWords(params.Body)
+	cleaned := cleanedBody{Clean: cleanedText}
+	respondWithJSON(w, http.StatusOK, cleaned)
+}
+
+func censorWords(input string) string {
+	wordsToCensor := map[string]bool{
+		"kerfuffle": true,
+		"sharbert":  true,
+		"fornax":    true,
+	}
+
+	split := strings.Fields(input)
+
+	for i, word := range split {
+		if _, exists := wordsToCensor[strings.ToLower(word)]; exists {
+			split[i] = "****"
+		}
+	}
+
+	return strings.Join(split, " ")
 }
